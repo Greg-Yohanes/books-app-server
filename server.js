1233ec1c3e4006b366171f19030dc389417c6542
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-12-20 10:56:17 -0800

    fixed spelling

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 'use strict'
 
+//these first three lines of code are to impliment our "dependencies" from package.json "express, cors", and "pg".
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 
-const app = experess();
+//APP Setup
+const app = express();
 const PORT = process.env.PORT;
+//this next line of coded added from scots lecture example
+const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-const client = new pg.Client(process.env.DATABASE_URL);
+//DATABASE setup.
+const client = new pg.Client(DATABASE_URL) //This is scotts code vs ours below
+// const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
 app.use(cors());
+//line above is a "middleware" that approves access. "cors" makes it completely open api. "white listed" is the term used, oppositie from "blacklisted".
 
+//app.get('/avi/vi1/books'...) #Time Stamp 2:30pm-2:43pm pt.1
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+//
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
@@ -32,5 +41,11 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 //Automatically sync git ACP's: Hit deploy tab, connect github, select organization from drop down menu, search repo name and connect!
 
+//TEST IN SERVER WITHOUT FRONT END
+//api.jquery.com
+//$.get(http://localhost:3000/avi/vi1/books')
+//.then(console.log)
+//TIMESTAMP 3:15 for 
+//terminal: curl "http url thing like above paste here no postophies" and will run your back end work
 
-
+//
